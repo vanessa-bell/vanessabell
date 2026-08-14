ae5d34bc6c810b884533d26bf587e551de920d10
Vanessa Bell <vanessa.bell14@gmail.com>
2026-08-14 15:15:52 -0700

    cleanup Link

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import SiteHeader from "../components/SiteHeader";
 import ContactForm from "../components/ContactForm";
 import { usePageMeta } from "../hooks/usePageMeta";
